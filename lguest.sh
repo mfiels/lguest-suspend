ce8c5b0cd7d710514ae2c0f64a117f942d2edc77
Paul Campbell <paul.campbell@stonybrook.edu>
2014-04-26 17:49:06 -0400

    Added rmmod to lguest.sh

--- lguest.sh
+++ lguest.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+rmmod lg
+
 insmod /home/mark/linux-3.11.0/drivers/lguest/lg.ko
 
 /home/mark/linux-3.11.0/tools/lguest/lguest 256 /home/mark/linux-3.11.0/vmlinux --tunnet=192.168.19.1 --block=/home/mark/hda.vdi --initrd=/boot/initrd.img-3.11.10.5-lguest root=/dev/vda1 init=/sbin/init
