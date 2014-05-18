8b7d7e573441a904659a0f4fa882c3143497b41a
Mark Fielbig <mfielbig@gmail.com>
2014-05-18 13:58:54 -0700

    Updating shell scripts to have new lguest kernel image name.

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ rmmod lg
 
 insmod /home/${SUDO_USER}/linux-3.11.0/drivers/lguest/lg.ko
 
-/home/${SUDO_USER}/linux-3.11.0/tools/lguest/lguest 256 /boot/vmlinuz-3.11.10.5-lguest --verbose --tunnet=192.168.19.1 --block=/home/${SUDO_USER}/hda.vdi --initrd=/boot/initrd.img-3.11.10.5-lguest --snapshot=hello --clean root=/dev/vda1 init=/sbin/init
+/home/${SUDO_USER}/linux-3.11.0/tools/lguest/lguest 256 /boot/vmlinuz-3.11.10.5-lguest+ --verbose --tunnet=192.168.19.1 --block=/home/${SUDO_USER}/hda.vdi --initrd=/boot/initrd.img-3.11.10.5-lguest+ --snapshot=hello --clean root=/dev/vda1 init=/sbin/init
